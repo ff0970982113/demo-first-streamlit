a48c5c95d855d92ac8189f9b2cbb446a2d5b23ae
ff0970982113 <ff0970982113@gmail.com>
2026-03-03 17:15:47 +0800

    Update SHEET_INPUT to point to a specific spreadsheet

--- crud-app.py
+++ crud-app.py
@@ -18,7 +18,7 @@ gc = init_connection()
 # ==========================================
 # 2. 開啟指定的試算表與工作表
 # ==========================================
-SHEET_INPUT = "試算表網址"
+SHEET_INPUT = "https://docs.google.com/spreadsheets/d/18-PeI0w5caLJSbrPXQiX_br9aLKLDMkM1rg9AHsJioA/edit?usp=sharing"
 WORKSHEET_NAME = "工作表1"
 
 try:
